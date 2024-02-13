81b93313bd5bdf72d2bbbc7abc59b4188a756cd8
prasad more <prasadmore422605@gmail.com>
2024-02-13 17:46:27 +0530

    add data

---
+++ sample.tf
@@ -0,0 +1,10 @@
+provider = "aws" {
+ region = "eu-east-1"
+}
+
+resource "aws_instance" "my-instance" {
+    ami = "ami-0c7217cdde317cfec"
+    instance_type = "t2.micro"
+    key_name = "univasal-key"
+    vpc_security_group_ids = ["sg-0f7ef2c962792ccbb"]
+}
