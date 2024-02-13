51aeaa142767f515bc3ef047493048de9181add7
prasad more <prasadmore422605@gmail.com>
2024-02-13 19:04:32 +0530

    up

--- sample.tf
+++ sample.tf
@@ -6,6 +6,6 @@ provider "aws" {
 resource "aws_instance" "my-instance" {
     ami = "ami-0c7217cdde317cfec"
     instance_type = "t2.micro"
-    key_name = "univasal-key"
+    key_name = "univesal_key"
     vpc_security_group_ids = ["sg-0f7ef2c962792ccbb"]
 }
