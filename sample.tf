6b19c0efc16448b7c4528dec62be9e615670f5c7
prasad more <prasadmore422605@gmail.com>
2024-02-13 23:32:45 +0530

    up

--- sample.tf
+++ sample.tf
@@ -15,18 +15,18 @@ resource "aws_security_group" "my_sg" {
     to_port = "22"
     cidr_blocks = ["0.0.0.0/0"]
    }
-}
+
    ingress {
-    procitotocol = "TCP"
+    protocol = "TCP"
     from_port = "80"
     to_port = "80"
-    cidr_block = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
    }
 
    egress {
     protocol = "-1"
-    from_port = "0"
-    to_port = "0"
+    from_port = 0
+    to_port = 0
     cidr_blocks = ["0.0.0.0/0"]
    }
  }  
