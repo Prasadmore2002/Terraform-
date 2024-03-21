09063a96432fc08946e9b976d62365431b599533
prasad more <prasadmore422605@gmail.com>
2024-03-22 00:11:59 +0530

    add

--- test.tf
+++ test.tf
@@ -2,8 +2,9 @@ provider "aws" {
     region = "us-east-1"
   
 }
+
 resource "aws_instance" "terraform1" {
-     ami = "ami-03c5a38d438cf3d3e"
-     instance_type = "t2-micro"
+     ami = "ami-080e1f13689e07408"
+     instance_type = "t2.micro"
      key_name = "n.verginia" 
        }  
